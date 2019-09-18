2f823b51705b71f6943c6c4ba3b96eae091bc2ea
Jason Santana <jsantana006@gmail.com>
2019-09-18 17:21:48 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,40 @@
+class Book 
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def title 
+    @title
+  end
+  
+  def author=(author)
+    @author = author 
+  end
+  
+  def author 
+    @author
+  end
+  
+  def page_count=(page_count)
+    @page_count
+  end
+  
+  def page_count
+    272
+  end
+  
+  def genre=(genre)
+    @genre
+  end
+  
+  def genre
+    "Mystery"
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
+end
 
